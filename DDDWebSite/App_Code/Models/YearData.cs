25f72ebf5d363fb133be53bf9b96cf64c20732fb
ShunkevichDV <shu.dv@tut.by>
2011-11-02 16:02:05 +0200

    change2

---
+++ DDDWebSite/App_Code/Models/YearData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for YearData
+/// </summary>
+public class YearData
+{
+    public String YearName { get; set; }
+    public String MonthName { get; set; }
+    public String DayName { get; set; }
+    public String Percent { get; set; }
+
+    public YearData()
+    {
+        YearName = "";
+        MonthName = "";
+        DayName = "";
+        Percent = "";
+    }
+}
